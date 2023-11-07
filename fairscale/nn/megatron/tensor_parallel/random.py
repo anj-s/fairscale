01cbf643137eb109b73b744f19f4072c345fa6a5
Anjali Sridhar <anjali@anjalis-mbp.lan>
2023-11-07 09:34:34 -0800

    remove moe mapping

--- fairscale/nn/megatron/tensor_parallel/random.py
+++ fairscale/nn/megatron/tensor_parallel/random.py
@@ -175,10 +175,10 @@ def model_parallel_cuda_manual_seed(seed):
     # and model parallel state.
     _CUDA_RNG_STATE_TRACKER.add(_MODEL_PARALLEL_RNG_TRACKER_NAME, tensor_model_parallel_seed)
 
-    expert_parallel_seed = (
-        seed + 100 * get_expert_model_parallel_rank() + get_tensor_model_parallel_rank()
-    )
-    _CUDA_RNG_STATE_TRACKER.add(_EXPERT_PARALLEL_RNG_TRACKER_NAME, expert_parallel_seed)
+    # expert_parallel_seed = (
+    #     seed + 100 * get_expert_model_parallel_rank() + get_tensor_model_parallel_rank()
+    # )
+    # _CUDA_RNG_STATE_TRACKER.add(_EXPERT_PARALLEL_RNG_TRACKER_NAME, expert_parallel_seed)
 
 
 class CheckpointFunction(torch.autograd.Function):
