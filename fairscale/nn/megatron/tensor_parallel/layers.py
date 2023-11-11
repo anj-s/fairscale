d052f0e69073d10d919f3cadb3bbf436f763eb56
Anjali Sridhar <anjali@anjalis-mbp.lan>
2023-11-11 11:15:44 -0800

    enable TP for non transformer blocks as well

--- fairscale/nn/megatron/tensor_parallel/layers.py
+++ fairscale/nn/megatron/tensor_parallel/layers.py
@@ -366,11 +366,11 @@ class LinearWithGradAccumulationAndAsyncCommunication(torch.autograd.Function):
         # clones it if it's not contiguous:
         # https://github.com/pytorch/pytorch/blob/c47cf9bc7f9e02f649ab4ed53fe4d35732c92ab6/torch/_refs/__init__.py#L2761
         grad_output = grad_output.contiguous()
-        print(f"grad_output.size {grad_output.size()}")
-        # Convert the tensor shapes to 2D for execution compatibility
-        grad_output = grad_output.view(
-            grad_output.shape[0] * grad_output.shape[1], grad_output.shape[2]
-        )
+        if len(grad_output.size()) == 3:
+            # Convert the tensor shapes to 2D for execution compatibility
+            grad_output = grad_output.view(
+                grad_output.shape[0] * grad_output.shape[1], grad_output.shape[2]
+            )
         total_input = total_input.view(
             total_input.shape[0] * total_input.shape[1], total_input.shape[2]
         )
