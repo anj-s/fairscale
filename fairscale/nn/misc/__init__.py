6c51561d39b42c61cdc78387e0ec5b1f6627ee3a
Anjali Sridhar <anjali@anjalis-mbp.lan>
2023-11-16 11:22:14 -0800

    Modify import

--- fairscale/nn/misc/__init__.py
+++ fairscale/nn/misc/__init__.py
@@ -5,7 +5,4 @@
 
 from typing import List
 
-from ..data_parallel.fsdp.flatten_params_wrapper import FlattenParamsWrapper, _enable_pre_load_state_dict_hook
-from ..data_parallel.fsdp.param_bucket import GradBucket, ParamBucket
-
 __all__: List[str] = []
